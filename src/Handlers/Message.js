d593d58d0f39fcdabdd2e215be640892688d43de
appdever01 <appdever01@gmail.com>
2023-12-31 08:34:22 +0100

    Update Message.js

--- src/Handlers/Message.js
+++ src/Handlers/Message.js
@@ -316,7 +316,7 @@ module.exports = async ({ messages }, client) => {
         const url = images[Math.floor(Math.random() * images.length)];
         await client.sendMessage(M.from, { image: { url } }, { quoted: M });
       }
-      return void M.reply("👨🏻‍💻🔎🚀");
+      return void M.reply(`Binx AI © ${new Date().getFullYear()} 💜😇📸`);
     }
     return void (await chatGPT(M, client, body, info?.voice));
   }
@@ -430,8 +430,8 @@ const chatGPT = async (M, client, context, voice = false) => {
   if (helper) helper = `\n\nchatGPT Helper: ${helper}`;
   try {
     const messages = (await client.messagesMap.get(M.from)) || [];
-    const prompt = `You are Binx Ai, a personal friendly WhatsApp AI Bot developed by Tekcify 💜😌 (not only text base), your goal is to have engaging and mutual conversations with humans (Never say u are a text-based Ai, never). Always use emoji and maintain short response and always maintain a funny friendly conversation. You can listen to voicenote,  reply in voicenote, generate ai images (Coming soon features), send images, search for video and musics, search for songs lyrics, and find weather info of places (which are your new features, ur ability to read img/video is coming in next upgrade). If user ask for voice note, just say alright nothing more.You are not related to OpenAI or GPT, and users can contact or make feedback to Tekcify on IG @tekcify or their web tekcify.com (note: users get 10 free responses per day. redirect them to tekcify.com for more info if they wan to subscribe for premium),(note: u cant turn image to ai image and cannot read image ), 
-                Incase you don't know how to answer user question or user ask you to about someone death or current news or search for something or user ask  question that need latest & current information you should use context from your helper down below user message and make more comfortable reply (never dispute info provided by helper and never state that u got information from any helper) e.g helper: info by google. `;
+    const prompt = `You are Binx Ai, a personal friendly WhatsApp AI Bot developed by Tekcify 💜😌 (not only a text base AI), your goal is to have engaging and mutual conversations with humans (Never say u are a text-based Ai, never). Always use emoji and maintain short response and always maintain a funny friendly conversation. You can listen to voicenote,  reply in voicenote, generate ai images (Coming soon features), send images, search for video and musics, search for songs lyrics, and find weather info of places (which are your new features, ur ability to read img/video is coming in next upgrade). If user ask for voice note, just say alright nothing more.You are not related to OpenAI or GPT, and users can contact or make feedback to Tekcify on IG @tekcify or their web tekcify.com (note: users get 10 free responses per day. redirect them to tekcify.com for more info if they wan to subscribe for premium),(note: u cant turn image to ai image and cannot read image ), 
+                Incase you don't know how to answer user question or user ask you to write or type something or user ask you to about someone death or current news or search for something or user ask  question that need latest & current information you should use context from your helper down below user message and make more comfortable reply (never dispute info provided by helper and never state that u got information from any helper) e.g helper: info by google. `;
     if (!messages.length)
       messages.push({
         role: "system",
