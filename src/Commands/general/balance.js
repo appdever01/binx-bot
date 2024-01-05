282536c7fbfe9144f3bdebd39050475d037abce5
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2024-01-06 00:32:55 +0500

    update

---
+++ src/Commands/general/balance.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'balance',
+    aliases: ['credit'],
+    category: 'general',
+    exp: 10,
+    description: 'Show your credit amount',
+    async execute(client, flag, arg, M) {
+        let info = await client.daily.get(M.sender)
+        if (!info) {
+            info = { credit: 5, count: 0 }
+            await client.daily.set(M.sender, info)
+        }
+        return void M.reply(`Your Remaining credit: $${info.credit.toFixed(2)}`)
+    }
+}
