05377fd42cd484a949815cb7ed18567729a374a8
appdever01 <appdever01@gmail.com>
2024-01-07 13:55:31 +0100

    s

--- src/Commands/utils/pdf.js
+++ src/Commands/utils/pdf.js
@@ -1,48 +1,44 @@
 const PDFDocument = require('pdfkit');
-const axios = require('axios');
 
 module.exports = {
-    name: 'topdf',
-    aliases: ['pdf'],
+    name: 'pdf',
+    aliases: ['itp'],
     category: 'utils',
     exp: 10,
     description: 'Converts multiple images to PDF',
     async execute(client, flag, arg, M) {
-        if (!M.quoted || (M.quoted && M.quoted.mtype !== 'imageMessage'))
-            return M.reply('*Quote the images that you want to convert to PDF!*');
+        if (!client.pdfAPI) return M.reply("You didn't provide a PDF API key");
         
-        const imageMessages = [M.quotedMsg, M];
+        const content = JSON.stringify(M.quoted);
+        const isQuoted = M.type === 'extendedTextMessage' && content.includes('imageMessage');
+        const isImage = isQuoted ? M.type === 'extendedTextMessage' && content.includes('imageMessage') : M.type === 'imageMessage';
+        
+        if (!isImage) return M.reply("You didn't provide any images");
+        
+        const images = isQuoted ? await M.quoted.downloadAll() : await M.downloadAll();
         const pdfDoc = new PDFDocument();
         
+        images.forEach((image) => {
+            pdfDoc.image(image);
+        });
         
-
-        // Define the downloadImage function in client.utils
-        client.utils.downloadImage = async (imageMessage) => {
-        const imageUrl = imageMessage.message.imageMessage.url;
-        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-        return Buffer.from(response.data, 'binary');
-        };
-        try {
-            for (let i = 0; i < imageMessages.length; i++) {
-                const imageBuffer = await client.utils.downloadImage(imageMessages[i]);
-                pdfDoc.image(imageBuffer);
+        const pdfBuffer = await new Promise((resolve) => {
+            const buffers = [];
+            pdfDoc.on('data', (buffer) => buffers.push(buffer));
+            pdfDoc.on('end', () => resolve(Buffer.concat(buffers)));
+            pdfDoc.end();
+        });
+        
+        await client.sendMessage(
+            M.from,
+            {
+                document: pdfBuffer,
+                mimetype: 'application/pdf',
+                filename: 'converted_images.pdf'
+            },
+            {
+                quoted: M
             }
-            
-            const pdfBuffer = await new Promise((resolve, reject) => {
-                const buffers = [];
-                pdfDoc.on('data', (buffer) => buffers.push(buffer));
-                pdfDoc.on('end', () => resolve(Buffer.concat(buffers)));
-                pdfDoc.end();
-            });
-            
-            await client.sendMessage(
-                M.from,
-                { document: pdfBuffer, mimetype: 'application/pdf' },
-                { quoted: M }
-            );
-        } catch (error) {
-            client.log(error, 'red');
-            return await M.reply('*Try Again*');
-        }
+        );
     }
-}
+};
--- src/Commands/utils/removebg.js
+++ src/Commands/utils/removebg.js
@@ -1,44 +1,28 @@
-const PDFDocument = require('pdfkit');
-
 module.exports = {
-    name: 'imagesToPdf',
-    aliases: ['itp'],
+    name: 'removebg',
+    aliases: ['rbg'],
     category: 'utils',
     exp: 10,
-    description: 'Converts multiple images to PDF',
+    description: 'Removes background from the image',
     async execute(client, flag, arg, M) {
-        if (!client.pdfAPI) return M.reply("You didn't provide a PDF API key");
-        
-        const content = JSON.stringify(M.quoted);
-        const isQuoted = M.type === 'extendedTextMessage' && content.includes('imageMessage');
-        const isImage = isQuoted ? M.type === 'extendedTextMessage' && content.includes('imageMessage') : M.type === 'imageMessage';
-        
-        if (!isImage) return M.reply("You didn't provide any images");
-        
-        const images = isQuoted ? await M.quoted.downloadAll() : await M.downloadAll();
-        const pdfDoc = new PDFDocument();
-        
-        images.forEach((image) => {
-            pdfDoc.image(image);
-        });
-        
-        const pdfBuffer = await new Promise((resolve) => {
-            const buffers = [];
-            pdfDoc.on('data', (buffer) => buffers.push(buffer));
-            pdfDoc.on('end', () => resolve(Buffer.concat(buffers)));
-            pdfDoc.end();
-        });
-        
+        if (!client.bgAPI) return M.reply("You didn't provide an api key")
+        const content = JSON.stringify(M.quoted)
+        const isQuoted = M.type === 'extendedTextMessage' && content.includes('imageMessage')
+        const isImage = isQuoted
+            ? M.type === 'extendedTextMessage' && content.includes('imageMessage')
+            : M.type === 'imageMessage'
+        if (!isImage) return M.reply("You didn't provide an image")
+        const buffer = isQuoted ? await M.quoted.download() : await M.download()
+        const image = await client.utils.removeBG(buffer)
+
         await client.sendMessage(
             M.from,
             {
-                document: pdfBuffer,
-                mimetype: 'application/pdf',
-                filename: 'converted_images.pdf'
+                image: image
             },
             {
                 quoted: M
             }
-        );
+        )
     }
-};
+}
