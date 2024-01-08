bea29f73f61b2bcf82e9f645724f4bad47ec51b8
appdever01 <appdever01@gmail.com>
2024-01-09 00:31:05 +0100

    Update Plagiarism.js

--- src/Commands/utils/Plagiarism.js
+++ src/Commands/utils/Plagiarism.js
@@ -8,9 +8,9 @@ module.exports = {
     description: "Detect, if using someone else's work without giving them proper credit",
     async execute(client, flag, context, M) {
         const content = context || (M.quoted && M.quoted.text)
-        if (!content) return void M.reply('Provide the text you want to check as plagiarism!')
+        if (!content) return void M.reply('Provide the text you want to check as plagiarism, Baka!')
         const info = await hooke.autoCitation({ text: content, replace: true })
-        const original = !info?.split('Bibliography')?.[1]?.trim() ? ': None' : '';
+        const original = !info.split('Bibliography')[1].trim() ? ': None' : ''
         return void M.reply(info.trim() + original)
     }
 }
