d7d49770b74533a4a2eb691067fd82940aa9adc6
appdever01 <appdever01@gmail.com>
2024-01-10 01:32:07 +0100

    m

--- src/Commands/utils/Plagiarism.js
+++ src/Commands/utils/Plagiarism.js
@@ -1,16 +1,20 @@
-const hooke = require('hookejs')
+const hooke = require("hookejs");
 
 module.exports = {
-    name: 'plagiarism',
-    aliases: ['plagarism','check'],
-    category: 'utils',
-    exp: 100,
-    description: "Detect, if using someone else's work without giving them proper credit",
-    async execute(client, flag, context, M) {
-        const content = context || (M.quoted && M.quoted.text)
-        if (!content) return void M.reply('Provide the text you want to check as plagiarism, Baka!')
-        const info = await hooke.autoCitation({ text: content, replace: true })
-        const original = !info.split('Bibliography')[1].trim() ? ': None' : ''
-        return void M.reply(info.trim() + original)
-    }
-}
+  name: "plagiarism",
+  aliases: ["plagarism"],
+  category: "utils",
+  exp: 100,
+  description:
+    "Detect, if using someone else's work without giving them proper credit",
+  async execute(client, flag, context, M) {
+    const content = context || (M.quoted && M.quoted.text);
+    if (!content)
+      return void M.reply("Provide the text you want to check as plagiarism!");
+    const info = await hooke.autoCitation({ text: content, replace: true });
+    const original = !info.split("Bibliography")[1].trim() ? ": None" : "";
+    const replacedInfo = info.replace("Bibliography", "*Source found 🔎📝*");
+
+    return void M.reply(`*Plagarism Found* ✅‼️✅\n\n` + replacedInfo);
+  },
+};
